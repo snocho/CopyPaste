7d515840724061211726d74a5aeb73ad5b0ed823
Michal Snochowski <michal.snochowski@intel.com>
2015-01-30 22:08:58 +0100

    clean up again
    Signed-off-by: Michal Snochowski <snochowski@gmail.com>

--- Models/CopyPasteItem.cs
+++ Models/CopyPasteItem.cs
@@ -106,6 +106,7 @@ namespace CopyPaste.Models
             list.Insert(0, this);
             return;
         }
+
         #region INotifyPropertyChanged Members
 
         public event PropertyChangedEventHandler PropertyChanged;
